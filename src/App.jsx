2e41dec9101506333465336b9ae061cc3ea8ba8e
Christian <chrufes@yahoo.com.br>
2022-06-28 15:13:52 -0300

    CSS Modules

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
+import { Header } from './components/Header'
 import { Post } from './Post'
+
+import './styles.css'
 
 export function App() {
   return (
     <div>
+      <Header />
       <Post
         author="Christian"
         content="Teste"
