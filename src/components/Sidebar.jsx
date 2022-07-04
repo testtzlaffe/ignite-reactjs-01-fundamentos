6f920e44a47c1b3e590d60b904605d3221661b75
Christian <chrufes@yahoo.com.br>
2022-07-04 15:01:16 -0300

    Finalizando Sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,4 +1,6 @@
-import styles from './Sidebar.module.css'
+import { PencilLine } from 'phosphor-react';
+
+import styles from './Sidebar.module.css';
 
 export function Sidebar() {
   return (
@@ -9,12 +11,20 @@ export function Sidebar() {
       />
 
       <div className={styles.profile}>
+        <img 
+          className={styles.avatar} 
+          src="https://github.com/testtzlaffe.png" 
+        />
+
         <strong>Christian</strong>
         <span>Web Developer</span>
       </div>
 
       <footer>
-        <a href="#">Editar seu perfil</a>
+        <a href="#">
+          <PencilLine size={20} />
+          Editar seu perfil
+        </a>
       </footer>
     </aside>
   );
