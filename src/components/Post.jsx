a8b569f5910fe9995453624e62641f1e387c6d2f
Christian <chrufes@yahoo.com.br>
2022-07-04 22:21:59 -0300

    Componente: Post

---
+++ src/components/Post.jsx
@@ -0,0 +1,27 @@
+import styles from './Post.module.css'
+
+export function Post() {
+  return (
+    <article className={styles.post}>
+      <header>
+        <div className={styles.author}>
+          <img className={styles.avatar} src="https://github.com/testtzlaffe.png" />
+          <div className={styles.authorInfo}>
+            <strong>Christian</strong>
+            <span>Web Developer</span>
+          </div>
+        </div>
+
+        <time title="11 de maio às 08:00" dateTime='2022-05-11 08:00:00'>
+          Publicado há 1h
+        </time>
+      </header>
+
+      <div className={styles.content}>
+        <p>Fala galera</p>
+        <p><a href="">https://site.com</a></p>
+        <p><a href="">#novoprojeto</a></p>
+      </div>
+    </article>
+  );
+}
