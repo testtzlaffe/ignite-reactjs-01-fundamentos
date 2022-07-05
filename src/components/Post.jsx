eefba7fa9c2480d65e96cc513c8eca125398cbf4
Christian <chrufes@yahoo.com.br>
2022-07-05 19:16:45 -0300

    Formulario de comentarios

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -25,6 +25,16 @@ export function Post() {
           <a href="">#nlw</a>
         </p>
       </div>
+
+      <form className={styles.commentForm}>
+        <strong>Deixe seu feedback</strong>
+        <textarea 
+          placeholder='Deixe um comentário'
+        />
+        <footer>
+          <button type="submit">Publicar</button>
+        </footer>
+      </form>
     </article>
   );
 }
